c7ad2fc24d6e0030da7167445fe439e120f6167a
Joshua Flark <joshuajmark@gmail.com>
2019-02-21 17:45:59 -0800

    I take it back, timeouts in sidekiq are a bad idea

--- lib/ruby_html_tree_differ.rb
+++ lib/ruby_html_tree_differ.rb
@@ -1,15 +1,12 @@
 class RubyHtmlTreeDiffer
   require 'pycall/import'
-  require 'timeout'
   include PyCall::Import
 
   def initialize
     pyimport :htmltreediff
   end
 
-  def diff!(old_document, new_document, timeout = 0)
-    Timeout.timeout(timeout) do
-      htmltreediff.diff(old_document, new_document, pretty: true)
-    end
+  def diff!(old_document, new_document)
+    htmltreediff.diff(old_document, new_document, pretty: true)
   end
 end
