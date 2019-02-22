ee7438b37fda31a7fd66c6013029d04b0f9d7516
Joshua Flark <joshuajmark@gmail.com>
2019-02-21 16:24:43 -0800

    Allow optional timeout

--- lib/ruby_html_tree_differ.rb
+++ lib/ruby_html_tree_differ.rb
@@ -1,12 +1,15 @@
 class RubyHtmlTreeDiffer
   require 'pycall/import'
+  require 'timeout'
   include PyCall::Import
 
   def initialize
     pyimport :htmltreediff
   end
 
-  def diff!(old_document, new_document)
-    htmltreediff.diff(old_document, new_document, pretty: true)
+  def diff!(old_document, new_document, timeout = 0)
+    Timeout.timeout(timeout) do
+      htmltreediff.diff(old_document, new_document, pretty: true)
+    end
   end
 end
